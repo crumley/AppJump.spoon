ce0438fc21a17647fe223f22b29a02b9a705ed7b
Ryan Crumley <ryan.crumley@shopify.com>
2024-01-31 11:05:51 -0600

    Summon works like a toggle

--- init.lua
+++ init.lua
@@ -16,6 +16,7 @@ m.homepage = "https://github.com/Hammerspoon/Spoons"
 
 m.logger = logger.new('AppJump', 'debug')
 m.previousWindow = nil
+m.originalWindowSpace = {}
 
 -- Settings
 
@@ -49,13 +50,13 @@ function m:jump(filter)
 end
 
 function m:summon(filter)
+  local currentWindow = hs.window.focusedWindow()
   local newWindow = filter:getWindows(filter.sortByFocused)[1]
+
   if newWindow == nil then
     m.logger.d('Filter had no windows to jump to', filter)
     return
   end
-
-  local currentWindow = hs.window.focusedWindow()
 
   m.logger.d('AppJump:summon', newWindow)
   m.logger.d('Current', currentWindow)
@@ -63,17 +64,26 @@ function m:summon(filter)
   m.logger.d('newWindow == currentWindow', newWindow == currentWindow)
   m.logger.d('----')
 
-  if m.previousWindow ~= nil and newWindow == currentWindow then
-    m.previousWindow:focus()
-    m.previousWindow = currentWindow
-    return
-  end
-
+  local newWindowId = newWindow:id()
   local currentSpaceId = hs.spaces.focusedSpace()
-  hs.spaces.moveWindowToSpace(newWindow, currentSpaceId)
+  local windowSpaces = hs.spaces.windowSpaces(newWindow)
 
-  -- TODO if already on this space put it back where it was
-
+  if hs.fnutils.contains(windowSpaces, currentSpaceId) then
+    -- The window is on the current space, send it back home
+    local originalSpaces = m.originalWindowSpace[newWindowId]
+    if originalSpaces ~= nil then
+      hs.spaces.moveWindowToSpace(newWindow, originalSpaces[1])
+      m.originalWindowSpace[newWindowId] = nil
+      if m.previousWindow then
+        m.previousWindow:focus()
+      end
+      return
+    end
+  else
+    -- Move the window to the current space
+    m.originalWindowSpace[newWindowId] = windowSpaces
+    hs.spaces.moveWindowToSpace(newWindow, currentSpaceId)
+  end
   m.previousWindow = currentWindow
   newWindow:focus()
 end
